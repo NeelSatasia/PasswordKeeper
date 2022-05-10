ce77a6b46dc00d83249af06e89fd44f813144d94
NeelSatasia <69377597+NeelSatasia@users.noreply.github.com>
2022-05-10 00:12:46 -0500

    Added command to change encryption key

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from datamanager import *
+from accountinfo import AccountInfo
 from getpass import getpass
 import random
 from simple_term_menu import TerminalMenu
@@ -15,6 +16,7 @@ change_username_command = 'Change Username'
 change_password_command = 'Change Password'
 change_login_password_command = 'Change Login Password'
 forgot_password = 'Forgot Login Password'
+change_encryption_key = 'Change Encryption Key'
 cancel_a_command = '/cancel'
 close_program = 'Close'
 
@@ -31,24 +33,29 @@ characters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', '
               '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
               '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', ',', '.', '/', '?', '-', '_', '+', '=', ' ', '/', ":", ";"]
 
+def make_encryption_key(key):
+    encrypted_key = ''
+
+    for encrypted_character_index in range(len(key)):
+        encrypted_key += (' ' * (characters.index(key[encrypted_character_index]) + 1))
+
+        if encrypted_character_index < len(key) - 1:
+            encrypted_key += '\t'
+
+    return encrypted_key
+
+
 encryption_key = []
 
 
 if len(get_password_keeper_info(1)) == 0:
-    for character in characters:
-        encryption_key.append(character)
+    encryption_key = characters.copy()
 
     random.shuffle(encryption_key)
 
-    encrypted_key = ''
+    key = make_encryption_key(encryption_key)
 
-    for encrypted_character_index in range(len(encryption_key)):
-        encrypted_key += (' ' * (characters.index(encryption_key[encrypted_character_index]) + 1))
-
-        if encrypted_character_index < len(encryption_key) - 1:
-            encrypted_key += '\t'
-
-    add_password_keeper_info(encrypted_key)
+    add_password_keeper_info(key)
 
 else:
     uploaded_encrypted_key = get_password_keeper_info(1)
@@ -141,7 +148,7 @@ else:
 
 
 commands_options = [add_account, remove_account, remove_all_accounts, access_account, access_all_account_names, change_account_name_command, change_username_command, change_password_command,
-                   change_login_password_command, close_program]
+                   change_login_password_command, change_encryption_key, close_program]
 
 commands_menu = TerminalMenu(commands_options)
 
@@ -151,7 +158,6 @@ if logged_in == True:
     while True:
 
         commands_menu_index = commands_menu.show()
-        print()
 
         if commands_options[commands_menu_index] ==  add_account:
 
@@ -227,6 +233,7 @@ if logged_in == True:
 
                 input_account_name = input('\tAccount Name: ')
                 print()
+
 
                 if input_account_name != cancel_a_command:
 
@@ -482,15 +489,57 @@ if logged_in == True:
                             encrypted_new_password = encrypt(input_new_password)
 
                             change_password_keeper_info(2, encrypted_new_password)
-
+                            print()
                         else:
-                            print('\t\t(Passwords do not match!)\n')
+                            print('\n\t\t(Passwords do not match!)\n')
 
                     else:
-                        print('\t\t(Must enter a password!)\n')
+                        print('\n\t\t(Must enter a password!)\n')
 
                 else:
-                    print('\t\t(Password entered is incorrect!)\n')
+                    print('\n\t\t(Password entered is incorrect!)\n')
+
+
+
+
+        elif commands_options[commands_menu_index] == change_encryption_key:
+            input_login_password = getpass('Enter password: ')
+            print()
+
+            if len(input_login_password) > 0:
+                encrypted_login_password = encrypt(input_login_password)
+
+                if encrypted_login_password == failed_to_encrypt:
+                    print('\t' + failed_to_encrypt + '\n')
+
+                elif encrypted_login_password == get_password_keeper_info(2):
+
+                    accounts = []
+
+                    for stored_account_name in get_all_account_names():
+                        stored_account_info = get_account_info(stored_account_name)
+                        accounts.append(AccountInfo(decrypt(stored_account_name), decrypt(stored_account_info[0]), decrypt(stored_account_info[1])))
+
+                    encryption_key.clear()
+
+                    encryption_key = characters.copy()
+
+                    random.shuffle(encryption_key)
+
+                    new_key = make_encryption_key(encryption_key)
+
+                    change_password_keeper_info(1, new_key)
+                    change_password_keeper_info(2, encrypt(input_login_password))
+
+                    remove_all_accounts_info()
+
+                    for accountinfo in accounts:
+                        add_row(encrypt(accountinfo.account_name), encrypt(accountinfo.username), encrypt(accountinfo.password))
+
+                    print('\t(Encryption Key is changed through randomization!)\n')
+
+                else:
+                    print('\t(Password entered is incorrect!)\n')
 
 
 
